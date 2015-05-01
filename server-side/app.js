207d5da33b2da4f5b42834238ec44afe27ed90cd
Nonemoticoner <nonemoticoner@gmail.com>
2015-05-01 18:07:52 +0200

    Classes got more code
    Slowly givin' it a progress.

--- server-side/app.js
+++ server-side/app.js
@@ -17,7 +17,7 @@ class Ship{
 	constructor (x, y, orientation, length) {
 		this.x =x;
 		this.y =y;
-		this.or = orientation;	// vertical - true, horizontal - false
+		this.orientation = orientation;	// vertical - true, horizontal - false
 		this.length = length;
 		this.hp =[];
 
@@ -47,6 +47,92 @@ class Ship{
 		return true;
 	}
 
+}
+
+class Set{
+	constructor(ships =[]){
+		//0 empty space
+		//1 ship
+		//2 killed ship
+		//3 missed shot
+		this.map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0].
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+		this.ships = ships;
+
+		// locates all ships on map
+		for (var i = this.ships.length - 1; i >= 0; i--) {
+			this.locate(this.ships[i])
+		}
+	}
+
+	locate(ship){
+		if(ship.orientation == 'v'){
+			for(int i =0; i <ship.length; i++){
+				try{
+					if(this.map[ship.y +i][ship.x] ==0)
+						this.map[ship.y +i][ship.x] =1;
+					else
+						throw new Error("Ship you are trying to locate collides with another ship");
+				}
+				catch(error){
+					console.log(error);
+					return 1;
+				}
+			}
+		}
+		else if(ship.orientation == 'h'){
+			for(int i =0; i <ship.length; i++){
+				try{
+					if(this.map[ship.y][ship.x +i] ==0)
+						this.map[ship.y][ship.x +1] =1;
+					else
+						throw new Error("Ship you are trying to locate collides with another ship");
+				}
+				catch(error){
+					console.log(error);
+					return 1;
+				}
+			}
+		}
+	}
+
+
+}
+
+
+class Player{
+	constructor(nick, status ="pending", set =new Set()){
+		this.nick =nick;
+		this.set =set;
+		this.status = status;	// pending or playing
+	}
+
+	attackedBy(opponent, x, y){
+		if(this.set.map[y][x] ==0){
+			this.set.map[y][x] =3;
+			return 0;
+		}
+		else if(this.set.map[y][x] ==1){
+			this.set.map[y][x] =2;
+			return 1;
+		}
+		else if(this.set.map[y][x] ==2)
+			return 2;
+		else if(this.set.map[y][x] ==3)
+			return 3;
+		else
+			return -1;		//fail
+	}
+
+	
 }
 
 
