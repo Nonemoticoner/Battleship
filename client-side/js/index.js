bacb44925eff03aec3bfd11e6bb741055e3c458c
Nonemoticoner <nonemoticoner@gmail.com>
2015-05-28 19:39:31 +0200

    Index.js
    Requires improvement for CORS - I'll do that later

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,5 +1,25 @@
 (function () {
+
+	var address = "http://nonemoticoner.asd-ent.pl:3001";
 	
-	// script for index.html here
+	var join = $("button#submit"),
+		form = $("input");
+
+	join.on('click', function (e) {
+		e.preventDefault();
+
+		var nick = form[0].value;
+
+		$.ajax({
+			type: "POST",
+			url: address + "/register",
+			data: {
+				nick: nick
+			}
+		}).done(function (res) {
+			console.log(res);
+			alert("sent");
+		});
+	});
 
 })();
