b28812d450a55f775f6834381b6e31daa6da574a
Nonemoticoner <nonemoticoner@gmail.com>
2015-06-21 12:49:36 +0200

    Few bugs fixed
    Playing is actually possible now

--- client-side/js/game.js
+++ client-side/js/game.js
@@ -59,6 +59,8 @@
 
 			battle = new Battle(res.attacker.nick, res.defender.nick, [att, def]);
 
+			battle.turn = parseInt(res.turn);
+
 			// bring back ints
 			for (var i = 0; i < 10; i++)
 				for (var j = 0; j < 10; j++){
@@ -95,9 +97,9 @@
 				for (var i = 0; i < 10; i++)
 					for (var j = 0; j < 10; j++)
 						if(battle.defender.set.map[i][j] == 2)
-							$(nick_map[i][j].context.firstChild).addClass("glyphicon-remove");
+							$(opponent_map[i][j].context.firstChild).addClass("glyphicon-remove");
 						else if(battle.defender.set.map[i][j] == 3)
-							$(nick_map[i][j].context.firstChild).addClass("glyphicon-certificate");
+							$(opponent_map[i][j].context.firstChild).addClass("glyphicon-certificate");
 
 			}
 			else{
@@ -115,9 +117,9 @@
 				for (var i = 0; i < 10; i++)
 					for (var j = 0; j < 10; j++)
 						if(battle.attacker.set.map[i][j] == 2)
-							$(nick_map[i][j].context.firstChild).addClass("glyphicon-remove");
+							$(opponent_map[i][j].context.firstChild).addClass("glyphicon-remove");
 						else if(battle.attacker.set.map[i][j] == 3)
-							$(nick_map[i][j].context.firstChild).addClass("glyphicon-certificate");
+							$(opponent_map[i][j].context.firstChild).addClass("glyphicon-certificate");
 			}
 
 			// check if my turn
@@ -148,33 +150,42 @@
 		//1 ship
 		//2 killed ship
 		//3 missed shot
-		switch( isAttacker ? battle.defender.attackedBy(battle.attacker, x, y) : battle.attacker.attackedBy(battle.defender, x, y)){
-			case 0:	// miss
-				$($(this).context.firstChild).addClass("glyphicon-certificate");
+		
+		var didCorrectShot = false, canHitAgain = false;
 
-				// disable 'Enemy Ships'
-				for (var i = 0; i < 100; i++) {
-					$($("#opponent table button")[i]).addClass("disabled");
-				}
-				break;
-			case 1:	// ship hit
-				$($(this).context.firstChild).addClass("glyphicon-remove");
+		while(!didCorrectShot){
+			switch( isAttacker ? battle.defender.attackedBy(battle.attacker, x, y) : battle.attacker.attackedBy(battle.defender, x, y)){
+				case 0:	// miss
+					$($(this).context.firstChild).addClass("glyphicon-certificate");
 
-				// disable 'Enemy Ships'
-				for (var i = 0; i < 100; i++) {
-					$($("#opponent table button")[i]).addClass("disabled");
-				}
-				break;
-			case 2:	// already hit
-				alert("You can't shoot in the same place again!");
-				break;
-			case 3:	// already missed
-				alert("You can't miss in the same place again!");
-				break;
+					// disable 'Enemy Ships'
+					for (var i = 0; i < 100; i++) {
+						$($("#opponent table button")[i]).addClass("disabled");
+					}
+					didCorrectShot = true;
+					break;
+				case 1:	// ship hit
+					$($(this).context.firstChild).addClass("glyphicon-remove");
+
+					// disable 'Enemy Ships'
+					for (var i = 0; i < 100; i++) {
+						$($("#opponent table button")[i]).addClass("disabled");
+					}
+					didCorrectShot = true;
+					canHitAgain = true;
+					break;
+				case 2:	// already hit
+					alert("You can't shoot in the same place again!");
+					break;
+				case 3:	// already missed
+					alert("You can't miss in the same place again!");
+					break;
+			}
 		}
-
+		
 		// increse turn
-		battle.turn = parseInt(battle.turn)+1;
+		if(!canHitAgain)
+			battle.turn = parseInt(battle.turn)+1;
 	
 		// send updated battle object to server
 		$.ajax({
